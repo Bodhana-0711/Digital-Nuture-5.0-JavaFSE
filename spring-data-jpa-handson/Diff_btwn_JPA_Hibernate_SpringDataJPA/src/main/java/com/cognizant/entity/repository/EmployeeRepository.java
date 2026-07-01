ed4e756c53a84680f163ba34d9a83a05f81da822
Bodhana-0711 <bodhanasrisaragadam@gmail.com>
2026-07-01 18:32:15 +0530

    Delete spring-data-jpa-handson/Diff_btwn_JPA_Hibernate_SpringDataJPA directory

--- spring-data-jpa-handson/Diff_btwn_JPA_Hibernate_SpringDataJPA/src/main/java/com/cognizant/entity/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cognizant.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
